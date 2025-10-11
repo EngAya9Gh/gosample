7a4c6fd17f8509f3cc633ef24ce9f6e6e1b57ffa
Elit ITSC <gneemamjad@outlook.com>
2025-10-11 12:51:50 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
 
                             <div class="card-body p-4">
                                 <div class="text-center mt-2">
-                                    <h5 class="text-primary">Welcome Back !</h5>
+                                    <h5 class="text-primary">Welcome !</h5>
                                     <p class="text-muted">Sign in to continue to @lang('translation.appname').</p>
                                 </div>
                                 <div class="p-2 mt-4">
