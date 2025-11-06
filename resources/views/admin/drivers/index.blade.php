09afc52102f7a74c3e28c996e7146d78f96f13b4
Elit ITSC <gneemamjad@outlook.com>
2025-11-06 04:04:51 +0300

    Update index.blade.php

--- resources/views/admin/drivers/index.blade.php
+++ resources/views/admin/drivers/index.blade.php
@@ -286,7 +286,7 @@
         </div>
     </div>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <!-- <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css"> -->
     <script>
         // Modal + Sortable Tasks Logic
         $(document).on('click', '.view-tasks', function() {
