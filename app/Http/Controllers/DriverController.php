72502bd90514c72cbd258090907d6da211298a29
Amjad Ghneim <gneemamjad@outlook.com>
2025-12-21 13:41:11 +0300

    Update DriverController.php

--- app/Http/Controllers/DriverController.php
+++ app/Http/Controllers/DriverController.php
@@ -896,7 +896,7 @@ class DriverController extends Controller
             $driver = Driver::where('id', $data['driver_id'])->first();
             $car = Car::where('id', $data['car_id'])->first();
             if(isset($driver) && isset($car)) {
-                // إنشاء أو تحديث حالة الطوارئ
+                $message = '🚨 تنبيه طارئ من السائق #' . $driver->name . ' في السيارة #' . $car->plate_number;
                 EmergencyFlag::updateOrCreate(
                     ['driver_id' => $data['driver_id'], 'active' => true],
                     [
