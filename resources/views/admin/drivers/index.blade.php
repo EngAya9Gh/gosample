3d56e3dc9dc8eeb4a026012cb146e28bd89c8694
Amjad Ghneim <gneemamjad@outlook.com>
2025-12-24 17:01:18 +0300

    Update index.blade.php

--- resources/views/admin/drivers/index.blade.php
+++ resources/views/admin/drivers/index.blade.php
@@ -63,7 +63,7 @@
                                 <label for="statusInput">{{ trans('translation.task.fields.status') }}</label>
                                 <select class="form-control" name="status" id="statusInput">
                                     <option value="1">Enable</option>
-                                    <option value="0">Disable</option>
+                                    <option value="2">Disable</option>
                                 </select>
                             </div>
 
